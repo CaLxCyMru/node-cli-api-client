dbca98498ab6b5e4695aef0a41a7204b372c4eb8
BuildTools <unconfigured@null.spigotmc.org>
2019-05-31 12:58:30 +0200

    servo now a module & refactored

---
+++ commands/servo.js
@@ -0,0 +1,40 @@
+const req = require('../requests');
+const Slider = require('../slider.js');
+const ora = require('ora');
+
+module.exports = {
+    name: 'servo',
+    description: 'Renders a slider for chaning ',
+    usage: '[command name]',
+    execute(args) {
+        const spinner = ora('Getting Servo current value').start();
+        req.get('http://zbestsolutions.it:5000/api/servo')
+            .then(function (response) {
+                spinner.stop();
+                const slider = new Slider(response, 360);
+
+                slider.on('value', (value) => {
+                    req.post('http://zbestsolutions.it:5000/api/servo', { "servo": value })
+                        .then(function (parsedBody) {
+                            console.log(`Value Changed to ${value}`);
+                            process.exit();
+                        });
+                })
+                if (args.l) {
+                    slider.on('changed', (value) => {
+                        req.post('http://zbestsolutions.it:5000/api/servo', { "servo": value })
+                            .then(function (parsedBody) {
+                                // Post went thru
+                            }).catch(err => {
+                                console.log(err.message);
+                                process.exit();
+                            })
+                    });
+                }
+            })
+            .catch(function (err) {
+                spinner.fail()
+                console.log(err.message);
+            });
+    },
+};
