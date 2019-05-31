e54ee8aead46d6b7b371468d29f807f7f8eeb8cc
Callum Morris <me@callum.io>
2019-05-31 18:21:38 +0100

    Showing an updated example

--- commands/led.js
+++ commands/led.js
@@ -1,27 +1,32 @@
+// Could change this to imports
 const req = require('../lib/requests');
-module.exports = {
+
+const execute = async (args) => {
+    const color = args._[1];
+
+    if (!colour) {
+       console.log('todo: keywords w/ autocomplete');
+       return;   
+    }
+
+    if (color.search(/^[0-9a-f]{6}$/i) === -1) {
+        return;
+    }
+
+    try { 
+         const response = await req.post('zbestsolutions.it:5000/api/rgb', { "rgb" : `#${color}` });
+        console.log(`Color changed to ${response.rgb}`);
+    } catch(err) { 
+        console.log(err.message);
+    }
+
+    process.exit();
+};
+
+export default {
     name: 'led',
     description: 'Changes the color of the led thru keywords or hex values',
     usage: '[command name] or shorform [command name] hexvalue without the #',
     argsUsage: [],
-    execute(args) {
-        const color = args._[1];
-        
-        if (color) {
-            if (color.search(/^[0-9a-f]{6}$/i) != -1) {
-                req.post('zbestsolutions.it:5000/api/rgb', { "rgb" : `#${color}` })
-                .then(response =>{
-                    console.log(`Color changed to ${response.rgb}`);
-                    process.exit();
-                })
-                .catch(err =>{
-                    console.log(err.message);
-                    process.exit();
-                })
-            }
-        } else{
-            //keywords
-            console.log('todo: keywords w/ autocomplete');
-        }
-    },
+    execute,
 };
