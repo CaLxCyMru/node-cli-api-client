c34f95c7395d85377838df4ac9b668664a3a68e5
BuildTools <unconfigured@null.spigotmc.org>
2019-05-31 12:59:50 +0200

    designed zapi.js as a command loader/selector

---
+++ zapi.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
+
+const file = fs.readdirSync('./commands').filter(file => file == `${argv._[0]}.js`)[0];
+if (file) {
+    const command = require(`./commands/${file}`);
+    command.execute(argv);
+    
+} else{
+    console.log(`Command "${argv._[0]}" not found.`);
+}
